f713bf16abd58ad3b0b1e7d513ee420c973ae658
filip <filip.wielkopolan@gmail.com>
2024-01-25 17:25:36 +0100

    Removed @Test (test is based on live info and would fail in the future)

--- src/test/java/pl/wielkopolan/flightscraper/services/impl/HomepagePromotionListServiceTest.java
+++ src/test/java/pl/wielkopolan/flightscraper/services/impl/HomepagePromotionListServiceTest.java
@@ -1,6 +1,22 @@
 package pl.wielkopolan.flightscraper.services.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.json.JSONArray;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
 class HomepagePromotionListServiceTest {
-  
+
+    @Autowired
+    HomepagePromotionListService homepagePromotionListService = new HomepagePromotionListService();
+
+    @Test
+    void justDummyGetPromotionList() throws IOException {
+        JSONArray promotionList = homepagePromotionListService.getPromotionList();
+        System.out.println("promotionList = " + promotionList);
+    }
+
 }
--- src/test/java/pl/wielkopolan/flightscraper/services/impl/RainbowFlightConnectionInfoServiceTest.java
+++ src/test/java/pl/wielkopolan/flightscraper/services/impl/RainbowFlightConnectionInfoServiceTest.java
@@ -1,7 +1,35 @@
 package pl.wielkopolan.flightscraper.services.impl;
 
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RainbowFlightConnectionInfoServiceTest {
+    //magic numbers - represent real live data from Rainbow Tours API , must be updated manually
+    public static final int ID_OUTBOUND_FLIGHT = 170765;
+    public static final int ID_RETURN_FLIGHT = 305293;
+    @Autowired
+    RainbowFlightConnectionInfoService rainbowFlightConnectionInfoService = new RainbowFlightConnectionInfoService();
 
+    /**
+     * This test has to be updated with correct ID values,
+     * since they are tested over life environment and quickly become outdated.
+     */
+
+    void shouldReturnJsonForWorkingIds(){
+        //given
+        int idOutboundFlight = ID_OUTBOUND_FLIGHT;
+        int idReturnFlight = ID_RETURN_FLIGHT;
+        //when
+        JSONObject infoAboutFlightConnection = rainbowFlightConnectionInfoService.getInfoAboutFlightConnection(idOutboundFlight, idReturnFlight);
+        //then
+        System.out.println("FOUND infoAboutFlightConnection = " + infoAboutFlightConnection);
+        assertNotNull(infoAboutFlightConnection);
+    }
 }

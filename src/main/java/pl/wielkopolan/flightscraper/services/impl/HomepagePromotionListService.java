2dd6274419af2e3db3c25a09c4c1278b2a96f1dd
filip <filip.wielkopolan@gmail.com>
2024-02-19 00:21:56 +0100

    Added configurable search months threshold. Simplified evaluating dates when building search url.

--- src/main/java/pl/wielkopolan/flightscraper/services/impl/HomepagePromotionListService.java
+++ src/main/java/pl/wielkopolan/flightscraper/services/impl/HomepagePromotionListService.java
@@ -10,6 +10,8 @@ import pl.wielkopolan.flightscraper.util.JsonReader;
 import pl.wielkopolan.flightscraper.util.UrlReader;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 @Service
 public class HomepagePromotionListService implements PromotionListService {
@@ -18,6 +20,8 @@ public class HomepagePromotionListService implements PromotionListService {
 
     @Value("${rainbow.search.api.url.promotions}")
     private String promotionUrl;
+    @Value("${scraper.search.months.forward}")
+    private int monthsToAdd;
 
     @Override
     public JSONArray getPromotionList() throws JSONException {
@@ -26,8 +30,8 @@ public class HomepagePromotionListService implements PromotionListService {
 
     private String prepareUrl() {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
-        String firstDateOfMonth= formatter.format(DateUtils.getFirstDayOfMonth());
-        String lastDateOfMonth= formatter.format(DateUtils.getLastDayOfMonth());
-        return promotionUrl + DATE_MIN_PARAM + firstDateOfMonth + DATE_MAX_PARAM + lastDateOfMonth;
+        String today = formatter.format(Calendar.getInstance().getTime());
+        String lastDateOfMonth = formatter.format(DateUtils.getLastDayOfMonth(LocalDate.now().plusMonths(monthsToAdd)));
+        return promotionUrl + DATE_MIN_PARAM + today + DATE_MAX_PARAM + lastDateOfMonth;
     }
 }
--- src/main/java/pl/wielkopolan/flightscraper/util/DateUtils.java
+++ src/main/java/pl/wielkopolan/flightscraper/util/DateUtils.java
@@ -2,31 +2,14 @@ package pl.wielkopolan.flightscraper.util;
 
 import java.time.LocalDate;
 import java.time.YearMonth;
-import java.util.Calendar;
 import java.util.Date;
 
 public final class DateUtils {
-    public static Date getFirstDayOfMonth() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.DAY_OF_MONTH, 1); // Set to the first day of the month
-        resetTimePart(calendar); // Reset time part to midnight
-        return calendar.getTime();
-    }
-
-    public static Date getLastDayOfMonth() {
-        // Get the first day of the next month
-        LocalDate firstDayOfNextMonth = LocalDate.now().plusMonths(1).withDayOfMonth(1);
-        // Get the last day of the next month using YearMonth class
-        YearMonth yearMonth = YearMonth.from(firstDayOfNextMonth);
+    public static Date getLastDayOfMonth(LocalDate localDate) {
+        YearMonth yearMonth = YearMonth.from(localDate);
         return java.sql.Date.valueOf(yearMonth.atEndOfMonth());
     }
 
-    private static void resetTimePart(Calendar calendar) {
-        calendar.set(Calendar.HOUR_OF_DAY, 0);
-        calendar.set(Calendar.MINUTE, 0);
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-    }
     private DateUtils() {
         //Util class - private constructor
     }
